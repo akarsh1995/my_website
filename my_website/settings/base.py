6b51dc489af4782cde993547f1c1df52d28372af
Akarsh <akarsh.1995.02@gmail.com>
2019-09-10 03:41:37 +0530

    boilerplate rem

--- my_website/settings/base.py
+++ my_website/settings/base.py
@@ -1,20 +1,14 @@
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.join(BASE_DIR, '../')
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DEBUG'))
 
-# Application definition
 
 BASE_URL = os.environ.get('BASE_URL')
 
@@ -63,7 +57,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'my_website.wsgi.application'
 
 # Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 if os.environ.get('USE_DB') != 'postgres':
     DATABASES = {
@@ -85,7 +78,6 @@ else:
     }
 
 # Password validation
-# https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -103,7 +95,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # Internationalization
-# https://docs.djangoproject.com/en/2.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -122,7 +113,6 @@ STATICFILES_DIRS = (
 )
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
 
@@ -140,5 +130,6 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 # celery setup
 CELERY_BROKER_URL = os.environ.get('BROKER_URL')
+
 # username to take into account
 USER_NAME = os.environ.get('AUTH_USERNAME')
