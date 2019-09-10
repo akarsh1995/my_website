d7a2c139c5e1a76a0c32bb732ed283f9cd8ba459
Akarsh <akarsh.1995.02@gmail.com>
2019-09-10 12:06:56 +0530

    func typo context processor

--- info/context_processors.py
+++ info/context_processors.py
@@ -2,11 +2,12 @@ from django.conf import settings
 
 from info.models import Profile
 
+
+def get_profile():
+    return Profile.objects.get(user__username=settings.USER_NAME)
+
+
 try:
-    def get_profile():
-        return Profile.objects.get(user__username=settings.USER_NAME)
-
-
     def profile(request):
         p = get_profile()
         return {
@@ -14,7 +15,7 @@ try:
         }
 
 except NotImplemented('DB not implemented'):
-    def get_profile():
+    def profile():
         return {
             'profile': None
         }
