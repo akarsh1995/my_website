9f53e92bca8c0dd76d0a7cbe7ea19cfa57985c89
Akarsh <akarsh.1995.02@gmail.com>
2019-09-11 17:36:18 +0530

    Storage backends in prod settings added

--- my_website/settings/prod.py
+++ my_website/settings/prod.py
@@ -11,8 +11,16 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-AWS_LOCATION = 'static'
 
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# Static storage
+AWS_STATIC_LOCATION = 'static'
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_STATIC_LOCATION)
+STATICFILES_STORAGE = 'my_website.storage_backends.StaticStorage'
 
+# Public media storage
+AWS_PUBLIC_MEDIA_LOCATION = 'media/public'
+DEFAULT_FILE_STORAGE = 'my_website.storage_backends.PublicMediaStorage'
+
+# Private media storage
+AWS_PRIVATE_MEDIA_LOCATION = 'media/private'
+PRIVATE_FILE_STORAGE = 'my_website.storage_backends.PrivateMediaStorage'
