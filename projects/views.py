86de02d14ef4fe9fd89e8554ba3861f965e203c7
Akarsh <akarsh.1995.02@gmail.com>
2019-09-14 00:44:41 +0530

    Project page grid structure changed. Project categories introduced on home.

--- projects/views.py
+++ projects/views.py
@@ -8,6 +8,16 @@ class ProjectListView(generic.ListView):
     template_name = 'projects/projects.html'
     ordering = ['-end_date']
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        ctx = super().get_context_data()
+        ctx['category'] = self.request.GET.get('category')
+        return ctx
+
+    def get_queryset(self):
+        if self.request.GET.get('category'):
+            return Project.objects.filter(category__name=self.request.GET.get('category'))
+        return super().get_queryset()
+
 
 class ProjectDetailView(generic.DetailView):
     template_name = 'projects/project_details.html'
